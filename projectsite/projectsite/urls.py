ab2cab3c1bb3b0ded20519a544059f2c26e59d44
Bea Beatriz Cervancia <202080454@psu.palawan.edu.ph>
2026-03-02 17:12:38 +0800

    orgmem

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
+from studentorg.views import (
+    HomePageView,
+    OrganizationList,
+    OrganizationCreateView,
+    OrganizationUpdateView,
+    OrganizationDeleteView,
+    OrgMemberList,
+    OrgMemberCreateView,
+    OrgMemberUpdateView,
+    OrgMemberDeleteView,
+)
 from studentorg import views
 urlpatterns = [
 path("admin/", admin.site.urls),
@@ -25,4 +35,10 @@ path('organization_list', OrganizationList.as_view(), name='organization-list'),
 path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
 path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
 path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+    
+    # organization members
+    path('orgmember_list', OrgMemberList.as_view(), name='orgmember-list'),
+    path('orgmember_list/add', OrgMemberCreateView.as_view(), name='orgmember-add'),
+    path('orgmember_list/<pk>', OrgMemberUpdateView.as_view(), name='orgmember-update'),
+    path('orgmember_list/<pk>/delete', OrgMemberDeleteView.as_view(), name='orgmember-delete'),
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,20 +1,56 @@
+from itertools import count
+from multiprocessing import context
+
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from studentorg.models import Organization
-from studentorg.forms import OrganizationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from studentorg.models import OrgMember, Organization, Student
+from studentorg.forms import OrganizationForm, OrgMemberForm
 from django.urls import reverse_lazy
+from django.db.models import Q
+from django.utils import timezone
 
-class HomePageView(ListView):
+class HomePageView(LoginRequiredMixin,ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+        return context
 
 class OrganizationList(ListView):
     model = Organization
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+    ordering = ["college__college_name","name"]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('q')
+
+        if query:
+            qs = qs.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+        return qs
 
 class OrganizationCreateView(CreateView):
     model = Organization
@@ -32,3 +68,43 @@ class OrganizationDeleteView(DeleteView):
     model = Organization
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
+
+
+class OrgMemberList(LoginRequiredMixin, ListView):
+    model = OrgMember
+    context_object_name = 'orgmember'
+    template_name = 'orgmember_list.html'
+    paginate_by = 10
+    ordering = ['-date_joined']
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            qs = qs.filter(
+                Q(student__firstname__icontains=query) |
+                Q(student__lastname__icontains=query) |
+                Q(organization__name__icontains=query)
+            )
+        return qs
+
+
+class OrgMemberCreateView(LoginRequiredMixin, CreateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'orgmember_form.html'
+    success_url = reverse_lazy('orgmember-list')
+
+
+class OrgMemberUpdateView(LoginRequiredMixin, UpdateView):
+    model = OrgMember
+    form_class = OrgMemberForm
+    template_name = 'orgmember_form.html'
+    success_url = reverse_lazy('orgmember-list')
+
+
+class OrgMemberDeleteView(LoginRequiredMixin, DeleteView):
+    model = OrgMember
+    template_name = 'orgmember_del.html'
+    success_url = reverse_lazy('orgmember-list')
+    
